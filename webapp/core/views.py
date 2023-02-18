d85cbbdf1f89bd9ac3c1bf59958968365b305a35
Mark Ruddy <1markruddy@gmail.com>
2023-02-18 12:55:05 +0000

    broken template

--- webapp/core/views.py
+++ webapp/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("TEST")
+    # template = loader.get_template('templates/index.html')
+    # latest_question_list = ["first", "second"]
+    # context = {
+    #        'latest_question_list': latest_question_list,
+    #    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', {
+        'test_data': 'some test data'
+        })
