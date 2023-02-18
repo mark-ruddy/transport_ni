981d42718b8b008a5c4b4fc6db07e275b5efd99d
Mark Ruddy <1markruddy@gmail.com>
2023-02-18 17:00:51 +0000

    start of api

---
+++ webapp/core/api_interact.py
@@ -0,0 +1,21 @@
+from django.db import Error
+from django.forms.forms import ErrorDict
+import requests
+
+api_url = "https://api.postcodes.io/postcodes"
+
+def query_postcode(postcode):
+    request_addr = f"{api_url}/{postcode}"
+    resp = requests.get(request_addr)
+    resp_json = resp.json()
+    if resp_json['status'] != 200:
+        raise Error
+    return resp_json
+
+def get_soa_of_postcode(postcode):
+    resp_json = query_postcode(postcode)
+    return get_soa_from_resp_json(resp_json)
+
+def get_soa_from_resp_json(resp_json):
+    res = resp_json['result']
+    return res['lsoa']
--- webapp/core/views.py
+++ webapp/core/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from .postcode import Postcode
+
+from core.api_interact import get_soa_of_postcode
 from .forms import PostCodeForm
+from .api_interact import *
 
 # Create your views here.
 
 def index(request):
     form = PostCodeForm(request.POST)
     if form.is_valid():
-        postcode = Postcode()
-        postcode.format_postcode(form.cleaned_data['postcode'])
+        soa = get_soa_of_postcode(form.cleaned_data['postcode'])
+        print(f"SOA: {soa}")
+
+        # TODO: parse CSV here with that soa
+
         return render(request, 'index.html', {
             'form': form,
             'postcode_output': True,
-            'postcode_formatted': postcode.formatted_postcode,
+            'postcode_formatted': "none",
         })
     else:
         form = PostCodeForm()
